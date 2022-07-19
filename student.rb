cb7d8859a8fdb75ce31172924337a02ea49bcdfe
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-07-19 08:52:37 +0200

    fix linter errors

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require_relative './person'
 
 # Student  inherits from Person 
 class Student < Person
-  def initialize(age, parent_permission: true, classroom, name = 'Unknown', )
+  def initialize(classroom, age, parent_permission: true,  name = 'Unknown')
     super(name, age,  parent_permission)
     @classroom = classroom
   end
--- teacher.rb
+++ teacher.rb
@@ -1,9 +1,9 @@
 require_relative './person'
 
 class Teacher < Person
-    def initialize(name = "Unknown", age, parent_permission: true, specialty)
+    def initialize(specialization, age, parent_permission: true, name = 'Unknown')
         super(name, age,  parent_permission)
-        @specialization = specialty
+        @specialization = specialization
     end
     
     def can_use_service?
