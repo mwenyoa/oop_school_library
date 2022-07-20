beee2a04fa64db8792b93260439d83014911a6eb
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-07-20 22:59:15 +0200

    Add correct name method and  inherit nemeable object behaviour

--- person.rb
+++ person.rb
@@ -1,10 +1,19 @@
-class Person
+require_relative 'nameable'
+require_relative 'decorator'
+
+class Person < Nameable
   # contructor
   def initialize(age, parent_permission: true, name: 'Unknown')
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
+  end
+
+  # correct name method
+  def correct_name
+    @name
   end
 
   # getters and setters
