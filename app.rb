b8af87c6cf4086db056b8531eb195d063acca4a2
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-07-23 16:28:16 +0200

    Add method to add rental

--- app.rb
+++ app.rb
@@ -92,3 +92,26 @@ def create_book
 
   puts 'Book created successfully'
 end
+
+# create rental
+def create_rental
+  if @books.size.zero?
+    puts 'No Books Available'
+  elsif @people.size.zero?
+    puts 'No Person Available'
+  else
+    puts 'Select a book from the following list by number'
+    @books.each_with_index { |book, index| puts "#{index}) Book Title: #{book.title}, Author: #{book.author}" }
+    rental_book = gets.chomp.to_i
+    puts 'Select a person from the following list by number (not id)'
+    @people.each_with_index do |person, index|
+      puts "#{index}) Name: #{person.name} Age: #{person.age} Id: #{person.id}"
+    end
+    rental_person = gets.chomp.to_i
+    puts 'Enter date in format YYYY-MM-DD'
+    date = convert_date(gets)
+    rental_detail = Rental.new(@people[rental_person], @books[rental_book], date)
+    @rentals.push(rental_detail)
+    puts 'Rental Successfully Created'
+  end
+end
