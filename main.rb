4c54fa7a2aa25c126eeff23aa188e61cc43c7523
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-07-25 17:24:46 +0200

    refactor hash menu list with symbols

--- main.rb
+++ main.rb
@@ -7,13 +7,13 @@ class App
     puts 'Please choose an option by entering a number: '
 
     @list = {
-      '1' => 'List all books',
-      '2' => 'List all people',
-      '3' => 'Create a person',
-      '4' => 'Create a book',
-      '5' => 'Create a rental',
-      '6' => 'List all rentals for a given person id',
-      '7' => 'Exit'
+      '1': 'List all books',
+      '2': 'List all people',
+      '3': 'Create a person',
+      '4': 'Create a book',
+      '5': 'Create a rental',
+      '6': 'List all rentals for a given person id',
+      '7': 'Exit'
     }
 
     @list.each do |index, string|
