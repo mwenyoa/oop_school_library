206dddb172c25b8ddd0830e6fa154d28dbf435f8
Bria222 <briantechologies.co.ke@gmail.com>
2022-07-27 07:18:03 +0300

    create a method to read books from a file

---
+++ file_handler.rb
@@ -0,0 +1,13 @@
+require_relative './app'
+
+def read_book(file)
+  puts ''
+  puts '####################*BOOKS*####################'
+  puts ''
+  file.each do |el|
+    book_title = el['value']['title']
+    book_author = el['value']['author']
+    puts "Book Title: #{book_title} Book Author: #{book_author}"
+    add_book(book_title, book_author)
+  end
+end
