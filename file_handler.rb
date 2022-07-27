390c32addee463558d23fce1d199236f74d2aef5
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-07-27 11:50:12 +0200

    fix linter errors

--- file_handler.rb
+++ file_handler.rb
@@ -1,9 +1,8 @@
 require_relative './app'
 
 def read_book(file)
-  
   puts "\n ####################*BOOKS*#################### \n"
- 
+
   file.each do |el|
     book_title = el['value']['title']
     book_author = el['value']['author']
@@ -12,26 +11,24 @@ def read_book(file)
   end
 end
 
-def read_people(file)
-    puts "\n ####################*PEOPLE*#################### \n" 
-case el['value']['type']
-when 'student'
+def read_people(_file)
+  puts "\n ####################*PEOPLE*#################### \n"
+  case el['value']['type']
+  when 'student'
     student_classroom = el['value']['classroom']
     student_age = el['value']['age']
-    student_name =el['value']['name']
-    has_permission = el['value']['has_permisssion']
+    student_name = el['value']['name']
+    has_permission = el['value']['has_permission']
     puts "classroom: #{student_classroom} Student Age #{student_age} Student Name #{student_name}"
-    add_student(student_classroom,student_age,student_name,has_permisssion)
-when 'Teacher'
+    add_student(student_classroom, student_age, student_name, has_permission)
+  when 'Teacher'
     teacher_specialization = el['value']['specialization']
     teacher_age = el['value']['age']
-    teacher_name= el['value']['name']
+    teacher_name = el['value']['name']
     puts "teacher_specialization: #{teacher_specialization} Teacher Age #{teacher_age} Teacher Name #{teacher_name}"
-    add_teacher(teacher_specialization,teacher_age,teacher_name)
+    add_teacher(teacher_specialization, teacher_age, teacher_name)
 
-     end
-    end
+  end
 end
 
 # handle rental
-
